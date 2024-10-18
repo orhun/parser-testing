16bc4569c52a879a31a98663c2df76fa39b29ed1
Arne Beer <contact@arne.beer>
2024-10-18 15:29:41 +0200

    Complete example chumsky parser for .mtree files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::{
     fs::{self},
     io::Read,
@@ -7,6 +8,7 @@ use anyhow::Result;
 use ariadne::{Color, Label, Report, ReportKind, Source};
 use chumsky::{prelude::*, text::ascii, Parser};
 use flate2::read::GzDecoder;
+use text::newline;
 
 fn main() -> Result<()> {
     let compressed = false;
@@ -23,8 +25,8 @@ fn main() -> Result<()> {
 
     //println!("{}", content);
 
-    let (json, errs) = parser().parse(content.trim()).into_output_errors();
-    println!("{:#?}", json);
+    let (ast, errs) = parser().parse(content.trim()).into_output_errors();
+    println!("{:#?}", ast);
     errs.into_iter().for_each(|e| {
         Report::build(ReportKind::Error, (), e.span().start)
             .with_message(e.to_string())
@@ -44,27 +46,98 @@ fn main() -> Result<()> {
 fn parser<'a>() -> impl Parser<'a, &'a str, Vec<Statement<'a>>, extra::Err<Rich<'a, char>>> {
     use Statement::*;
 
-    let mtree = just("#").then(ascii::keyword("mtree")).to(Init);
+    let mtree = just("#")
+        .then(ascii::keyword("mtree"))
+        .then_ignore(newline())
+        .to(Init);
+
+    let default_properties = choice((
+        ascii::keyword("uid")
+            .then(just('='))
+            .ignore_then(text::digits(10).to_slice())
+            .map(|s: &str| DefaultProperty::Uid(s.parse().unwrap())),
+        ascii::keyword("gid")
+            .then(just('='))
+            .ignore_then(text::digits(10).to_slice())
+            .map(|s: &str| DefaultProperty::Gid(s.parse().unwrap())),
+        ascii::keyword("mode")
+            .then(just('='))
+            .ignore_then(text::digits(8).to_slice())
+            .map(|s: &str| DefaultProperty::Mode(s)),
+        ascii::keyword("type")
+            .then(just('='))
+            .ignore_then(choice((
+                ascii::keyword("dir").to(PathType::Dir),
+                ascii::keyword("file").to(PathType::File),
+                ascii::keyword("link").to(PathType::Link),
+            )))
+            .map(DefaultProperty::Type),
+    ))
+    .padded()
+    .repeated()
+    .collect::<Vec<_>>();
 
     let set = just("/")
-        .ignored()
-        .then_ignore(ascii::keyword("set"))
-        .to(Set(Vec::new()));
+        .then(ascii::keyword("set"))
+        .ignore_then(default_properties)
+        .map(Set);
 
     let unset = just("/")
         .ignored()
         .then_ignore(ascii::keyword("unset"))
         .to(Unset(Vec::new()));
 
-    recursive(|raw| choice((mtree, set, unset)).repeated().collect())
+    let properties = choice((
+        ascii::keyword("mode")
+            .then(just('='))
+            .ignore_then(text::digits(8).to_slice())
+            .map(|s: &str| Property::Mode(s)),
+        ascii::keyword("sha256digest")
+            .then(just('='))
+            .ignore_then(text::digits(16).to_slice())
+            .map(|s: &str| Property::Sha256Digest(s)),
+        ascii::keyword("size")
+            .then(just('='))
+            .ignore_then(text::digits(10).to_slice())
+            .map(|s: &str| Property::Size(s.parse().unwrap())),
+        ascii::keyword("time")
+            .then(just('='))
+            .ignore_then(text::digits(10).to_slice())
+            .then_ignore(just('.'))
+            .then_ignore(just('0'))
+            .map(|s: &str| Property::Time(s.parse().unwrap())),
+        ascii::keyword("type")
+            .then(just('='))
+            .ignore_then(choice((
+                ascii::keyword("dir").to(PathType::Dir),
+                ascii::keyword("file").to(PathType::File),
+                ascii::keyword("link").to(PathType::Link),
+            )))
+            .map(Property::Type),
+        ascii::keyword("link")
+            .then(just('='))
+            .ignore_then(none_of(" ").repeated().to_slice())
+            .map(Property::Link),
+    ))
+    .padded()
+    .repeated()
+    .collect::<Vec<_>>();
+
+    let path = just(".")
+        .then(none_of(" ").repeated().to_slice())
+        .to_slice()
+        .then(properties)
+        .map(|(path, properties)| Path { path, properties });
+
+    recursive(|raw| choice((mtree, set, unset, path)).repeated().collect())
 }
 
 #[derive(Debug, Clone)]
 enum Statement<'a> {
     // The initial `#mtree` line at the top of the file
     Init,
-    Set(Vec<DefaultProperty>),
-    Unset(Vec<DefaultProperty>),
+    Set(Vec<DefaultProperty<'a>>),
+    Unset(Vec<DefaultProperty<'a>>),
     Path {
         path: &'a str,
         properties: Vec<Property<'a>>,
@@ -73,20 +146,22 @@ enum Statement<'a> {
 
 /// This type is used in `/set` and `/unset` commands to modify the currently active defaults.
 #[derive(Debug, Clone)]
-enum DefaultProperty {
+enum DefaultProperty<'a> {
     Uid(usize),
     Gid(usize),
-    Mode(usize),
+    Mode(&'a str),
+    Type(PathType),
 }
 
 /// This type is used in `/set` and `/unset` commands to modify the currently active defaults.
 #[derive(Debug, Clone)]
 enum Property<'a> {
-    Mode,
+    Mode(&'a str),
     Type(PathType),
     Size(usize),
     Link(&'a str),
     Sha256Digest(&'a str),
+    Time(usize),
 }
 
 // What kind of type is a path.
