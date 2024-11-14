c94b9710b11afcae7d1b15450e0304a2ef78d8a3
Arne Beer <contact@arne.beer>
2024-11-14 18:17:55 +0100

    WIP winnow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::{
     fs::{self},
     io::Read,
@@ -7,12 +8,17 @@ use anyhow::Result;
 use flate2::read::GzDecoder;
 
 fn main() -> Result<()> {
-    let gz_content = fs::read(".MTREE")?;
-    let mut decoder = GzDecoder::new(gz_content.as_slice());
+    let compressed = false;
+    let content = if compressed {
+        let gz_content = fs::read(".MTREE")?;
+        let mut decoder = GzDecoder::new(gz_content.as_slice());
 
-    let mut content = String::new();
-    decoder.read_to_string(&mut content).unwrap();
-    println!("{}", content);
+        let mut content = String::new();
+        decoder.read_to_string(&mut content).unwrap();
+        content
+    } else {
+        String::from_utf8_lossy(&fs::read(".MTREE.extracted")?).to_string()
+    };
 
     Ok(())
 }
