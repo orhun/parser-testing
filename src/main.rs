94bdb9a9ebabb0cb9fcfc7ed0af46bc8b208f092
Arne Beer <contact@arne.beer>
2024-11-14 23:19:35 +0100

    add: base structs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,50 @@ use std::{
 use anyhow::Result;
 use flate2::read::GzDecoder;
 
+/// Each line represents a line in a .MTREE file
+#[derive(Debug, Clone)]
+enum Statement<'a> {
+    /// The initial `#mtree` line at the top of the file
+    Init,
+    /// A `/set` command followed by some properties
+    Set(Vec<DefaultProperty<'a>>),
+    /// A `/unset` command followed by some properties
+    Unset(Vec<DefaultProperty<'a>>),
+    /// Any path statement followed by some properties
+    Path {
+        path: &'a str,
+        properties: Vec<Property<'a>>,
+    },
+}
+
+/// This type is used in `/set` and `/unset` commands to modify the currently active defaults.
+#[derive(Debug, Clone)]
+enum DefaultProperty<'a> {
+    Uid(usize),
+    Gid(usize),
+    Mode(&'a str),
+    Type(PathType),
+}
+
+/// This type is used in a [Path] line and defines some available properties for that path.
+#[derive(Debug, Clone)]
+enum Property<'a> {
+    Mode(&'a str),
+    Type(PathType),
+    Size(usize),
+    Link(&'a str),
+    Sha256Digest(&'a str),
+    Time(usize),
+}
+
+// What kind of type is a path.
+#[derive(Debug, Clone, Copy)]
+enum PathType {
+    Dir,
+    File,
+    Link,
+}
+
 fn main() -> Result<()> {
     let compressed = false;
     let content = if compressed {
