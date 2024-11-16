25b1c9d8d10a3052dddf33329f025cb49aa18352
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-11-16 10:49:37 +0300

    Add winnow parser PoC for mtree

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,22 @@ use std::{
 
 use anyhow::Result;
 use ariadne::{Color, Label, Report, ReportKind, Source};
-use chumsky::{prelude::*, text::ascii, Parser};
+use chumsky::{
+    prelude::*,
+    text::{ascii, newline},
+    Parser,
+};
 use flate2::read::GzDecoder;
-use text::newline;
+
+use winnow::{
+    ascii::{alpha1, alphanumeric1, line_ending, space0, space1},
+    combinator::{alt, delimited, preceded, repeat, separated_pair, terminated},
+    token::take_till,
+    PResult, Parser as WinnowParser,
+};
 
 /// Each line represents a line in a .MTREE file
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Statement<'a> {
     /// The initial `#mtree` line at the top of the file
     Init,
@@ -27,7 +37,7 @@ enum Statement<'a> {
 }
 
 /// This type is used in `/set` and `/unset` commands to modify the currently active defaults.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum DefaultProperty<'a> {
     Uid(usize),
     Gid(usize),
@@ -36,7 +46,7 @@ enum DefaultProperty<'a> {
 }
 
 /// This type is used in a [Path] line and defines some available properties for that path.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 enum Property<'a> {
     Mode(&'a str),
     Type(PathType),
@@ -47,54 +57,15 @@ enum Property<'a> {
 }
 
 // What kind of type is a path.
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum PathType {
     Dir,
     File,
     Link,
 }
 
-fn main() -> Result<()> {
-    let compressed = false;
-    // Either read the compressed or already uncompressed .MTREE file at the root of this
-    // repo and return the contents.
-    let content = if compressed {
-        let gz_content = fs::read(".MTREE")?;
-        let mut decoder = GzDecoder::new(gz_content.as_slice());
-
-        let mut content = String::new();
-        decoder.read_to_string(&mut content).unwrap();
-        content
-    } else {
-        String::from_utf8_lossy(&fs::read(".MTREE.extracted")?).to_string()
-    };
-
-    //println!("{}", content);
-
-    // Parse the file
-    let (ast, errs) = parser().parse(content.trim()).into_output_errors();
-
-    // Print out the AST
-    println!("{:#?}", ast);
-
-    // Print out any errors.
-    errs.into_iter().for_each(|e| {
-        Report::build(ReportKind::Error, (), e.span().start)
-            .with_message(e.to_string())
-            .with_label(
-                Label::new(e.span().into_range())
-                    .with_message(e.reason().to_string())
-                    .with_color(Color::Red),
-            )
-            .finish()
-            .print(Source::from(&content))
-            .unwrap()
-    });
-
-    Ok(())
-}
-
-fn parser<'a>() -> impl Parser<'a, &'a str, Vec<Statement<'a>>, extra::Err<Rich<'a, char>>> {
+fn parser_chumsky<'a>() -> impl Parser<'a, &'a str, Vec<Statement<'a>>, extra::Err<Rich<'a, char>>>
+{
     use Statement::*;
 
     // Parser for the very first line of the `.MTREE` file
@@ -203,4 +174,173 @@ fn parser<'a>() -> impl Parser<'a, &'a str, Vec<Statement<'a>>, extra::Err<Rich<
         .map(|(path, properties)| Path { path, properties });
 
     recursive(|_| choice((mtree, set, unset, path)).repeated().collect())
+}
+
+/// Parses a single value until a space or newline is encountered.
+fn value<'s>(i: &mut &'s str) -> PResult<&'s str> {
+    terminated(
+        take_till(0.., |c| c == ' ' || c == '\n'),
+        alt((space1, line_ending)),
+    )
+    .parse_next(i)
+}
+
+/// Parses a single value until a space or newline is encountered
+///
+/// This parser also consumes any trailing whitespace or newlines.
+fn value_terminated<'s>(i: &mut &'s str) -> PResult<&'s str> {
+    (value, alt((space0, line_ending)))
+        .parse_next(i)
+        .map(|(v, _)| v)
+}
+
+/// Parses the initial `#mtree` line at the top of the file.
+fn init<'s>(i: &mut &'s str) -> PResult<Statement<'s>> {
+    terminated("#mtree", line_ending)
+        .parse_next(i)
+        .map(|_| Statement::Init)
+}
+
+/// Parses a single default property.
+fn default_property<'s>(i: &mut &'s str) -> PResult<DefaultProperty<'s>> {
+    separated_pair(alpha1, "=", value)
+        .verify_map(|(k, v)| match k {
+            "uid" => Some(DefaultProperty::Uid(v.parse().ok()?)),
+            "gid" => Some(DefaultProperty::Gid(v.parse().ok()?)),
+            "type" => match v {
+                "dir" => Some(DefaultProperty::Type(PathType::Dir)),
+                "file" => Some(DefaultProperty::Type(PathType::File)),
+                "link" => Some(DefaultProperty::Type(PathType::Link)),
+                _ => panic!("unknown type: {v}"),
+            },
+            "mode" => Some(DefaultProperty::Mode(v)),
+            _ => panic!("unknown property: {k}"),
+        })
+        .parse_next(i)
+}
+
+/// Parses a list of default properties.
+fn default_properties<'s>(i: &mut &'s str) -> PResult<Vec<DefaultProperty<'s>>> {
+    repeat(0.., default_property).parse_next(i)
+}
+
+/// Parses a `/set` command followed by some properties.
+fn set<'s>(i: &mut &'s str) -> PResult<Statement<'s>> {
+    delimited(("/set", space0), default_properties, line_ending)
+        .parse_next(i)
+        .map(Statement::Set)
+}
+
+/// Parses a `/unset` command followed by some properties.
+fn unset<'s>(i: &mut &'s str) -> PResult<Statement<'s>> {
+    delimited(("/unset", space0), default_properties, line_ending)
+        .parse_next(i)
+        .map(Statement::Unset)
+}
+
+/// Parses a single property.
+fn property<'s>(i: &mut &'s str) -> PResult<Property<'s>> {
+    separated_pair(alphanumeric1, "=", value)
+        .verify_map(|(k, v)| match k {
+            "type" => match v {
+                "dir" => Some(Property::Type(PathType::Dir)),
+                "file" => Some(Property::Type(PathType::File)),
+                "link" => Some(Property::Type(PathType::Link)),
+                _ => panic!("unknown type: {v}"),
+            },
+            "mode" => Some(Property::Mode(v)),
+            "size" => Some(Property::Size(v.parse().ok()?)),
+            "link" => Some(Property::Link(v)),
+            "sha256digest" => Some(Property::Sha256Digest(v)),
+            "time" => Some(Property::Time(v.split_once(".")?.0.parse().ok()?)),
+            _ => panic!("unknown property: {k}"),
+        })
+        .parse_next(i)
+}
+
+/// Parses a list of properties.
+fn properties<'s>(i: &mut &'s str) -> PResult<Vec<Property<'s>>> {
+    repeat(0.., property).parse_next(i)
+}
+
+/// Parses a path line followed by some properties.
+fn path<'s>(i: &mut &'s str) -> PResult<Statement<'s>> {
+    let path = preceded(".", value).parse_next(i)?;
+    let properties = delimited(' ', properties, line_ending).parse_next(i)?;
+    Ok(Statement::Path { path, properties })
+}
+
+/// Parses the next statement in the file.
+fn statement<'s>(i: &mut &'s str) -> PResult<Statement<'s>> {
+    let statement_type: &str = alt((
+        (".", value).take(),
+        "/set ",
+        "/unset ",
+        terminated("#mtree", line_ending),
+    ))
+    .parse_next(i)?;
+
+    let statement = match statement_type {
+        "/set " => Statement::Set(default_properties(i)?),
+        "/unset " => Statement::Unset(default_properties(i)?),
+        "#mtree" => Statement::Init,
+        path => Statement::Path {
+            path,
+            properties: properties(i)?,
+        },
+    };
+
+    Ok(statement)
+}
+
+/// Parses the entire .MTREE file.
+fn parser<'s>(i: &mut &'s str) -> PResult<Vec<Statement<'s>>> {
+    repeat(0.., statement).parse_next(i)
+}
+
+fn main() -> Result<()> {
+    let compressed = false;
+    // Either read the compressed or already uncompressed .MTREE file at the root of this
+    // repo and return the contents.
+    let content = if compressed {
+        let gz_content = fs::read(".MTREE")?;
+        let mut decoder = GzDecoder::new(gz_content.as_slice());
+
+        let mut content = String::new();
+        decoder.read_to_string(&mut content).unwrap();
+        content
+    } else {
+        String::from_utf8_lossy(&fs::read(".MTREE.extracted")?).to_string()
+    };
+
+    match parser.parse_next(&mut content.as_str()) {
+        Ok(ast) => {
+            println!("{:#?}", ast);
+        }
+        Err(e) => {
+            eprintln!("{e}");
+        }
+    }
+
+    // Parse the file
+    let (_ast, errs) = parser_chumsky().parse(content.trim()).into_output_errors();
+
+    // Print out the AST
+    // println!("{:#?}", ast);
+
+    // Print out any errors.
+    errs.into_iter().for_each(|e| {
+        Report::build(ReportKind::Error, (), e.span().start)
+            .with_message(e.to_string())
+            .with_label(
+                Label::new(e.span().into_range())
+                    .with_message(e.reason().to_string())
+                    .with_color(Color::Red),
+            )
+            .finish()
+            .print(Source::from(&content))
+            .unwrap()
+    });
+
+    Ok(())
 }
